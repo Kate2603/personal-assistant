dca7354784bb534dcfe4661587746dfb2d391bc5
OlgaHumphreys <163923607+OlgaHumphreys@users.noreply.github.com>
2024-05-24 19:53:50 +0100

    Task has been finished 9.1 & 9.2

--- notebook/note.py
+++ notebook/note.py
@@ -1,7 +1,8 @@
 class Note:
-    def __init__(self, title, content):
+    def __init__(self, title, content, tags: list):
         self.title = title
         self.content = content
+        self.tags = tags
 
-    def __str__(self):
-        return f"{self.title}: {self.content}"
+    def __repr__(self):
+        return f"Title {self.title}\n Description {self.content}\nTags {', '.join(self.tags)} "
